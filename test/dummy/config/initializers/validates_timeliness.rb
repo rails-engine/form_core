d708d1ba1bff8c1f060ee31438f9da2c656751e5
jasl <jasl9187@hotmail.com>
2018-07-03 16:37:43 +0800

    update gems

--- test/dummy/config/initializers/validates_timeliness.rb
+++ test/dummy/config/initializers/validates_timeliness.rb
@@ -24,7 +24,7 @@ ValidatesTimeliness.setup do |config|
   # )
   #
   # Use the plugin date/time parser which is stricter and extendable
-  config.use_plugin_parser = true
+  # config.use_plugin_parser = false
   #
   # Add one or more formats making them valid. e.g. add_formats(:date, 'd(st|rd|th) of mmm, yyyy')
   # config.parser.add_formats()
